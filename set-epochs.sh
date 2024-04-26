9440263753dbb745269b58541824d428ecea58da
toninorair <antonina.norair@gmail.com>
2024-04-26 13:34:59 -0400

    Update to new ttg

--- set-epochs.sh
+++ set-epochs.sh
@@ -15,28 +15,35 @@ if [ "$profile" = "dry-run" ]; then
     timestamp=$(date +%s)
     timestamp=$(expr $timestamp - 86401)
     period="1 days"
+    periodInSecs=86400
 elif [ "$profile" = "staging" ]; then
     # Staging has 15 minute epochs
     timestamp=$(date +%s)
     timestamp=$(expr $timestamp - 901)
     period="15 minutes"
+    periodInSecs=900
 elif [ "$profile" = "dev" ]; then
     # Dev has 400 second epochs
     timestamp=$(date +%s)
     timestamp=$(expr $timestamp - 401)
     period="400 seconds"
+    periodInSecs=400
 elif [ "$profile" = "test" ]; then
     # Mainnet has 15 day epochs
     timestamp="1_710_171_999"
     period="15 days"
+    periodInSecs=1296000
 elif [ "$profile" = "production" ]; then
     # Mainnet has 15 day epochs
     timestamp=$(date +%s)
     timestamp=$(expr $timestamp - 1296001)
     period="15 days"
+    periodInSecs=1296000
 fi
 
 sed "s/.*constant STARTING_TIMESTAMP.*/    uint40 internal constant STARTING_TIMESTAMP = $timestamp;/" lib/ttg/src/libs/PureEpochs.sol > ./lib/ttg/src/libs/PureEpochs1.sol
 sed "s/.*constant EPOCH_PERIOD.*/    uint40 internal constant EPOCH_PERIOD = $period;/" lib/ttg/src/libs/PureEpochs1.sol > ./lib/ttg/src/libs/PureEpochs2.sol
-mv ./lib/ttg/src/libs/PureEpochs2.sol ./lib/ttg/src/libs/PureEpochs.sol
+sed "s/epochStartingTimestamp=[0-9]*/epochStartingTimestamp=$timestamp/; s/epochPeriod=[0-9]*/epochPeriod=$periodInSecs/" lib/ttg/src/libs/PureEpochs2.sol > ./lib/ttg/src/libs/PureEpochs3.sol
+mv ./lib/ttg/src/libs/PureEpochs3.sol ./lib/ttg/src/libs/PureEpochs.sol
 rm ./lib/ttg/src/libs/PureEpochs1.sol
+rm ./lib/ttg/src/libs/PureEpochs2.sol
